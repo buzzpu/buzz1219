6c2549a29fa10591472d374fcf2d055960ff9ba1
buzzpuapp <buzz.pu@gmail.com>
2015-12-20 11:32:45 +0800

    003

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var db =[];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -15,11 +17,18 @@ router.post('/1/post', function(req, res, next) {
     console.log( "title :" + title + "\n");
     console.log( "message :" + message + "\n");
     
+    var article = {
+        title : title,
+        message: message
+    }
+    
+    db.push(article);
+    res.end();
     
 });
 
 router.get('/1/post', function(req, res, next) {
-    
+    res.json(db)
 });
 
 
