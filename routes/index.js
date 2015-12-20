45d6b9088a2a600de11c342882768ca06261c8fc
buzzpuapp <buzz.pu@gmail.com>
2015-12-20 10:59:11 +0800

    002

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//Rest Api
+router.post('/1/post', function(req, res, next) {
+    var title = req.query.title;
+    var message = req.query.message;
+    
+    console.log( "title :" + title + "\n");
+    console.log( "message :" + message + "\n");
+    
+    
+});
+
+router.get('/1/post', function(req, res, next) {
+    
+});
+
+
+
 module.exports = router;
